fe96cb214d3f6a5cb4c7a0e50e15696019c73714
Ryan Brady <ryanbrady369@gmail.com>
2019-01-31 23:59:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,9 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
-      item = @@items.find{|item| item.name = item_name}
+      item = @@items.find{|item| item.name == item_name}
 
-      if item.nil?
+      if !item
         resp.write "Item not found"
         resp.status = 400
       else
